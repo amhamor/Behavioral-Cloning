0ef1366be10bfc5d4c2d193f07284f003b535d4c
amhamor <amhamor@gmail.com>
2018-01-01 15:04:44 -0600

    Updated files.

--- training_and_evaluation2.py
+++ training_and_evaluation2.py
@@ -45,6 +45,30 @@ def create_model(image_shape):
 
 	model.add(Dense(output_dim=4096, activation='linear'))
 	model.add(BatchNormalization())
+	model.add(Dropout(0.50))
+
+	model.add(Dense(output_dim=2048, activation='tanh'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.50))
+
+	model.add(Dense(output_dim=1024, activation='linear'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.50))
+
+	model.add(Dense(output_dim=512, activation='tanh'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.50))
+
+	model.add(Dense(output_dim=256, activation='linear'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.50))
+
+	model.add(Dense(output_dim=128, activation='tanh'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.50))
+
+	model.add(Dense(output_dim=64, activation='linear'))
+	model.add(BatchNormalization())
 	#model.add(Dropout(0.50))
 
 	#Output layer:
