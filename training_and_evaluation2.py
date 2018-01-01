206c81f38004c46097b8882a2a124b737d03f546
amhamor <amhamor@gmail.com>
2018-01-01 14:58:20 -0600

    Updated files.

--- training_and_evaluation2.py
+++ training_and_evaluation2.py
@@ -24,10 +24,14 @@ def create_model(image_shape):
 	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), border_mode='same'))
 	model.add(BatchNormalization())
 
+	model.add(Convolution2D(nb_filter=512, nb_row=3, nb_col=3, activation='relu', border_mode='same', input_shape=image_shape))
+	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), border_mode='same'))
+	model.add(BatchNormalization())
+
 	#Classification layers:
 	model.add(Flatten())
 
-	model.add(Dense(output_dim=512, activation='linear'))
+	model.add(Dense(output_dim=1024, activation='linear'))
 	model.add(BatchNormalization())
 	#model.add(Dropout(0.50))
 
