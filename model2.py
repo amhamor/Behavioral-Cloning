0bef1fc353d805519721edc6b77061385e2f0b7f
amhamor <amhamor@gmail.com>
2018-01-01 13:39:27 -0600

    Updated files.

--- model2.py
+++ model2.py
@@ -13,10 +13,10 @@ training_driving_log_file_path_list = ['./training_data/training_driving_log.csv
 
 #validation_driving_log_file_path = './training_data/validation_driving_log.csv'
 
-weights_file_path_list = ['model_checkpoint_training_driving_log.csv_1_0.16980559089078187.hdf5']
+weights_file_path_list = ['']
 #weights_file_path_list = ['model_checkpoint_sharp-left-curve-after-bridge.csv_4_0.5708236373156286.hdf5', 'model_checkpoint_straight-after-sharp-left-curve.csv_4_0.36227246174541466.hdf5', 'model_checkpoint_sharp_right_curve.csv_4_0.47088188685064536.hdf5', 'model_checkpoint_straight-after-sharp-right-curve.csv_4_0.2827446764173294.hdf5']
 batch_size = 64
-initial_epoch = 2
+initial_epoch = 0
 nb_epoch = 10000
 
 for training_driving_log_file_path, weights_file_path in zip(training_driving_log_file_path_list, weights_file_path_list):
