29ad12964e8d397e2784ca0b02b98422333319a1
amhamor <amhamor@gmail.com>
2018-01-02 00:00:59 -0600

    Updated files.

---
+++ data_generation3.py
@@ -0,0 +1,22 @@
+import csv
+import numpy as np
+from scipy.ndimage import imread
+from image_processor import *
+
+def get_image_and_steering_angle_data(csv_file_path):
+	with open(training_driving_log_file_path, 'rb') as csv_file_object:
+		image_data = []
+		steering_angle_data = []
+		header_and_offset_list = [('Center Camera Images', 0), ('Left Camera Images', 0.1), ('Right Camera Images', -0.1)]
+		dict_reader = csv.DictReader(csv_file_object)
+		for row in dict_reader:
+			for header, offset in header_and_offset_list:
+				image = imread(fname=row['Center Camera Images'], mode='L')
+				image = crop_image(image=image)
+				image = normalize_image(image=image)
+				image = np.expand_dims(image, axis=2)
+				image_data.append(image)
+
+				steering_angle_data.append(row['Steering Angle'] + offset)
+
+	return (image_data, steering_angle_data)
