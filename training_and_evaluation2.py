557b3a7e5e6576270c19b4c4be025340c984b4b4
amhamor <amhamor@gmail.com>
2018-01-01 19:26:49 -0600

    Updated files.

--- training_and_evaluation2.py
+++ training_and_evaluation2.py
@@ -52,11 +52,11 @@ def create_model(image_shape):
 	#model.add(BatchNormalization())
 	#model.add(Dropout(0.50))
 
-	model.add(Dense(output_dim=1024, activation='tanh'))
+	#model.add(Dense(output_dim=1024, activation='tanh'))
 	#model.add(BatchNormalization())
 	#model.add(Dropout(0.50))
 
-	model.add(Dense(output_dim=512, activation='linear'))
+	#model.add(Dense(output_dim=512, activation='linear'))
 	#model.add(BatchNormalization())
 	#model.add(Dropout(0.50))
 
