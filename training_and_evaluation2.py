f423d32cd4f168a11beba46bb73dc61e3acf2f45
amhamor <amhamor@gmail.com>
2018-01-03 03:31:15 -0600

    Five Dense layers with alternating tanh and linear activation functions.

--- training_and_evaluation2.py
+++ training_and_evaluation2.py
@@ -21,9 +21,11 @@ def create_model(image_shape):
 	#Classification layers:
 	model.add(Flatten())
 
+	model.add(Dense(output_dim=225, activation='tanh'))
+	model.add(Dense(output_dim=160, activation='linear'))
 	model.add(Dense(output_dim=100, activation='tanh'))
 	model.add(Dense(output_dim=50, activation='linear'))
-	model.add(Dense(output_dim=10, activation='linear'))
+	model.add(Dense(output_dim=10, activation='tanh'))
 
 	#Output layer:
 	model.add(Dense(output_dim=1, activation='linear'))
